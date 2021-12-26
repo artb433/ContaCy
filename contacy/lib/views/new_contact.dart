a8e3001e4481187d287e352fa599c5f5bcecd08d
Art b <94374097+artb433@users.noreply.github.com>
2021-12-26 03:23:24 +0000

    my

---
+++ contacy/lib/views/new_contact.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewContact extends StatelessWidget {
+  const NewContact({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
